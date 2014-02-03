d3dc5b5deec3a139edac4a3882fc3b79909ba17c
Thomas Christensen <thomas@chrstnsn.dk>
2014-02-03 22:27:19 +0100

    reflect actual github url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gyag/lein-template "0.1.1-SNAPSHOT"
   :description "Opinionated template for creating web applications in Clojure using ClojureScript and Ring"
-  :url "http://github.com/thomaschrstnsn/gyag"
+  :url "http://github.com/thomaschrstnsn/gyag-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
