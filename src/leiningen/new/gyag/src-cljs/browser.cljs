190e4f2caeff4b694195540a17b41859e3f32b7a
Thomas Christensen <thomas@chrstnsn.dk>
2014-02-05 23:24:50 +0100

    add clojure.browser.repl to clojurescript file

--- src/leiningen/new/gyag/src-cljs/browser.cljs
+++ src/leiningen/new/gyag/src-cljs/browser.cljs
@@ -1,4 +1,5 @@
-(ns {{name}}.browser)
+(ns {{name}}.browser
+    (:require [clojure.browser.repl :as repl]))
 
 (defn ^:export clojurescript-main []
   (.log js/console "Hello world! From ClojureScript!"))
