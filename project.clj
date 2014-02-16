153709488112ad834de3ffdfa342e196a0d23185
Thomas Christensen <thomas@chrstnsn.dk>
2014-02-16 14:00:23 +0100

    lein-release plugin: bumped version from 0.9.0 to 0.9.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gyag/lein-template "0.9.0"
+(defproject gyag/lein-template "0.9.1-SNAPSHOT"
   :description "Opinionated template for creating web applications in Clojure using ClojureScript and Ring"
   :url "http://github.com/thomaschrstnsn/gyag-template"
   :license {:name "Eclipse Public License"
